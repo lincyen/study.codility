2a22702bc606c5c61e00cd4598c8434f92ccb875
winterforest <winterforest@DESKTOP-CS9JFP7>
2022-04-24 11:38:07 +0900

    initial

---
+++ study.codility/src/com/codility/app/test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.codility.app;
+
+/**
+ * @author winterforest
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
