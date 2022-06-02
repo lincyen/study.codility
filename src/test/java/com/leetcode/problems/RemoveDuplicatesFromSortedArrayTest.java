c3ddf739f64604f84c9c5e75d6e3e0b8f28f7144
Lincyen <lincyen@gmail.com>
2022-06-02 15:30:18 +0900

    MergeTwoSortedLists Push

--- src/test/java/com/leetcode/problems/RemoveDuplicatesFromSortedArrayTest.java
+++ src/test/java/com/leetcode/problems/RemoveDuplicatesFromSortedArrayTest.java
@@ -8,5 +8,8 @@ class RemoveDuplicatesFromSortedArrayTest {
 
     @Test
     void removeDuplicates() {
+        assertEquals(new RemoveDuplicatesFromSortedArray().removeDuplicates(new int[] {}), 0);
+        assertEquals(new RemoveDuplicatesFromSortedArray().removeDuplicates(new int[] {1,1,2}), 2);
+        assertEquals(new RemoveDuplicatesFromSortedArray().removeDuplicates(new int[] {0,0,1,1,1,2,2,3,3,4}), 5);
     }
 }
